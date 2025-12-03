8f9125c70a3470bc6bd8874a56fbb810ac6a7d65
wwsouthard <wsouthard@my365.bellevue.edu>
2025-12-02 18:55:12 -0600

    Hands-on completed

---
+++ week-6/project11-02/project11-02.js
@@ -0,0 +1,33 @@
+"use strict";
+/*    JavaScript 7th Edition
+     Chapter 11
+     Project 11-02
+
+     Project to city and state information from a provided postal code
+     Author: Will Southard 
+     Date: 12/02/2025
+
+     Filename: project11-02.js
+*/
+
+let postalCode = document.getElementById("postalCode");
+let place = document.getElementById("place");
+let region = document.getElementById("region");
+let country = document.getElementById("country");
+
+postalCode.onblur = function() {
+   let codeValue = postalCode.value;
+   let countryValue = country.value;
+   
+   place.value = "";
+   region.value = "";
+   
+   fetch(`http://api.zippopotam.us/${countryValue}/${codeValue}`)
+      .then(response => response.json())
+      .then(json => {
+         place.value = json.places[0]["place name"];
+         region.value = json.places[0]["state abbreviation"];
+      })
+      .catch(error => console.log(error));
+}
+
